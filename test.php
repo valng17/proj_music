404666d65b6fe231255de166aab429893a19078c
valng17 <valentin.nguyen@edu.itescia.fr>
2022-02-11 17:00:52 +0100

    php ok

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title> <?php echo "Premiers pas en PHP"; ?> </title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<h1> Mes premiers pas en PHP </h1>
+<?php $temps=2; echo "<p> Je débute depuis " ; $temps; " heures... </p>"; ?>
+<p> Mais cela a l’air intéressant <?php echo "!" ?> </p>
+<?php echo "
+<h2> Vive le PHP </h2>
+<p> Les pages vont pouvoir être dynamiques! </p>
+"; ?>
+<p> Encore quelques paragraphes </p>
+<?php echo "<p> Avant dernier paragraphe </p>"; ?>
+<p> Voilà, c’est terminé! </p>
+</body>
+</html>
